300f7caaa538d9d4e386c1346e9b744a41f5b3cb
Stas Lee <haraken769@gmail.com>
2017-12-16 17:41:17 -0500

    dwdw

--- src/Router.js
+++ src/Router.js
@@ -6,16 +6,19 @@ import LanguageForm from './components/LanguageForm'
 import RegistrationForm from './components/RegistrationForm'
 import ChoosingRoleForm from './components/ChoosingRoleForm'
 import UrgentSearch from './components/UrgentSearch'
+import FillingDoner from './components/FillingDoner'
 class RouterComponent extends Component {
   render() {
     return (
       <Router>
         <Scene  key='root'>
-        <Scene key='lang' hideNavBar={true} initial component={LanguageForm} />
-        <Scene key='login' hideNavBar={true} component={LoginForm} />
-        <Scene key='register'hideNavBar={false} component={RegistrationForm} />
-        <Scene key='choosingRoleForm'hideNavBar={true} component={ChoosingRoleForm} />
-        <Scene key='urgentSearch' hideNavBar={true} component= {UrgentSearch} />
+        <Scene key='lang' component={LanguageForm} />
+        <Scene key='login' component={LoginForm} />
+        <Scene key='register' component={RegistrationForm} />
+        <Scene key='choosingRoleForm' component={ChoosingRoleForm} />
+        <Scene key='urgentSearch'  component= {UrgentSearch} />
+        <Scene key='fillingDoner' initial  component= {FillingDoner} />
+        
         </Scene>
       </Router>
     )
