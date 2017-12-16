dc2dd570b42f7b54b3b92e49dbec0d80ed04e1a8
Stas Lee <haraken769@gmail.com>
2017-12-16 15:28:41 -0500

    login,reg,choosingRole,urgentSearch functional problems

--- src/Router.js
+++ src/Router.js
@@ -3,14 +3,19 @@ import { Scene, Router, Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
 import LoginForm from './components/LoginForm'
 import LanguageForm from './components/LanguageForm'
-
+import RegistrationForm from './components/RegistrationForm'
+import ChoosingRoleForm from './components/ChoosingRoleForm'
+import UrgentSearch from './components/UrgentSearch'
 class RouterComponent extends Component {
   render() {
     return (
       <Router>
-        <Scene key='root'>
+        <Scene hideNavBar={true} key='root'>
+        <Scene key='login' component={LoginForm} />
+        <Scene key='register' initial component={RegistrationForm} />
+        <Scene key='choosingRoleForm' component={ChoosingRoleForm} />
+        <Scene key='urgentSearch' component= {UrgentSearch} />
           <Scene key='lang' component={LanguageForm} />
-          <Scene key='login' component={LoginForm} title='Please Login'/>
         </Scene>
       </Router>
     )
--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -5,6 +5,7 @@ import { EMAIL_CHANGED,
     LOGIN_USER_SUCCESS,
     LOGIN_USER_FAIL,
     LOGIN_USER,
+    REGISTER_USER_FAIL,
 
 } from './types'
 
@@ -22,29 +23,43 @@ export const passwordChanged = text => {
     }
 }
 
-export const loginUser = ({ nickname, password }) => {
+export const loginUser = ({ email, password }) => {
     return dispatch => {
         dispatch({ type: LOGIN_USER})
-        firebase.auth().signInWithEmailAndPassword(nickname, password)
+        firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => loginUserSuccess(dispatch, user))
             .catch(() => {
-                firebase.auth().createUserWithEmailAndPassword(nickname, password)
-                    .then(user => loginUserSuccess(dispatch, user))
-                    .catch(() => loginUserFail(dispatch))
+                     loginUserFail(dispatch)
             })
     }
 }
-
+export const registerUser = ({email,password}) => {
+    return dispatch => {
+        dispatch({ type: LOGIN_USER})
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(() => { 
+            firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(user => loginUserSuccess(dispatch,user))
+            .catch(() => {
+                loginUserFail(dispatch)
+            })
+        })
+        .catch(()=>{
+            registerUser(dispatch)
+        })
+    }
+}
 const loginUserFail = (dispatch) => {
     dispatch({ type: LOGIN_USER_FAIL })
 }
-
+const registerUserFail = (dispatch) => {
+    dispatch({ type: REGISTER_USER_FAIL })
+}
 const loginUserSuccess = (dispatch, user) => {
     dispatch({
         type: LOGIN_USER_SUCCESS,
         payload: user
     })
-    console.log(Actions)
-    Actions.reportsOrEvacs({type: 'reset'})
+    Actions.choosingRoleForm({type : 'reset'})
 
 }
