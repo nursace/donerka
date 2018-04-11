05c9bde1a5c9f7c3d497d9522e9ae9153ac67b7b
Stas Lee <haraken769@gmail.com>
2018-04-11 11:08:36 -0400

    ern

---
+++ src/components/PrivatePolicy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+
+class PrivatePolicy extends Component{
+    render(){
+        return(
+            <View>
+                </View>
+        )
+    }
+}
+export default PrivatePolicy
