7008cfef325c700b79ff4790c9670f8f2ff6aa58
Sultan Ernisov <ernisov@gmail.com>
2018-04-11 21:15:08 +0600

    none

--- App.js
+++ App.js
@@ -11,17 +11,17 @@ import ReduxThunk from 'redux-thunk'
 export default class App extends React.Component {
     componentWillMount(){
         let config = {
-  apiKey: "AIzaSyBwoF-lXtFfYAYFYoti5cTAIxrpbT_nidg",
-      authDomain: "maps-f802a.firebaseapp.com",
-      databaseURL: "https://maps-f802a.firebaseio.com",
-      projectId: "maps-f802a",
-      storageBucket: "maps-f802a.appspot.com",
-      messagingSenderId: "465718551900",
+          apiKey: "AIzaSyBwoF-lXtFfYAYFYoti5cTAIxrpbT_nidg",
+          authDomain: "maps-f802a.firebaseapp.com",
+          databaseURL: "https://maps-f802a.firebaseio.com",
+          projectId: "maps-f802a",
+          storageBucket: "maps-f802a.appspot.com",
+          messagingSenderId: "465718551900",
         };
         firebase.initializeApp(config)
     }
   render() {
-    
+
     const store = createStore(reducer, {}, applyMiddleware(ReduxThunk))
     return (
         <Provider store={store}>
